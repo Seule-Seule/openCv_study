c4bb4b113f6630f9304807bc40f36ad14cfb353b
fanyucheng <fanyucheng@seule.cn>
2021-01-06 13:25:37 +0800

    FqImageMat update.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,91 +9,7 @@
 
 using namespace cv;
 
-QImage cvMatToQImage( const cv::Mat &inMat )
-{
-  switch ( inMat.type() )
-  {
-     // 8-bit, 4 channel
-     case CV_8UC4:
-     {
-        QImage image( inMat.data,
-                      inMat.cols, inMat.rows,
-                      static_cast<int>(inMat.step),
-                      QImage::Format_ARGB32 );
-
-        return image;
-     }
-
-     // 8-bit, 3 channel
-     case CV_8UC3:
-     {
-        QImage image( inMat.data,
-                      inMat.cols, inMat.rows,
-                      static_cast<int>(inMat.step),
-                      QImage::Format_RGB888 );
-
-        return image.rgbSwapped();
-     }
-
-     // 8-bit, 1 channel
-     case CV_8UC1:
-     {
-//         static QVector<QRgb>  sColorTable( 256 );
-
-//         only create our color table the first time
-//         注意：下面这种写法是错的，参考后文的说明
-//         如果将这个函数定义成一个类方法 那可以在图像没有改变时加快转换速度 现在这里是个函数 就不在加快转换速度
-//         且原函数在这个地方有bug sColorTable 定义时确定了大小， 默认初始化过了 第一次转换会出错 不能用 isEmpty()  判断是否第一次转换
-//              可以考虑初始化表值后 由值判断是否第一次转换
-//         if ( sColorTable.isEmpty() )
-//        {
-//           for ( int i = 0; i < 256; ++i )
-//           {
-//              sColorTable[i] = qRgb( i, i, i );
-//           }
-//        }
-
-        QVector<QRgb>  sColorTable( 256 );
-
-        for ( int i = 0; i < 256; ++i )
-        {
-           sColorTable[i] = qRgb( i, i, i );
-        }
-
-        QImage image( inMat.data,
-                      inMat.cols, inMat.rows,
-                      static_cast<int>(inMat.step),
-                      QImage::Format_Indexed8 );
-
-        image.setColorTable( sColorTable );
-
-        return image;
-     }
-
-     default:
-        qWarning() << "cvMatToQImage() - cv::Mat image type not handled in switch:" << inMat.type();
-        break;
-  }
-
-  return QImage();
-}
-
-void setLabelPixmap(QLabel * _qLanel, Mat & _cvMat, int _pWidth = 0, int _pHeight = 0)
-{
-    if (_pWidth == 0){
-        _pWidth = _cvMat.size().width;
-    }
-    if (_pHeight == 0){
-        _pHeight = _cvMat.size().height;
-    }
-    QImage tempQImage = cvMatToQImage(_cvMat);
-    QPixmap pixmap = QPixmap::fromImage(tempQImage);
-    pixmap= pixmap.scaled(_pWidth, _pHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-    _qLanel->setPixmap(pixmap);
-}
-
-
-void testMask(int type, Mat & _inMat, Mat& _outMat)
+void testMask(int type, const Mat & _inMat, Mat& _outMat)
 {
     if (type == 0){
         // Make mask by oneself.
@@ -127,16 +43,16 @@ void testMask(int type, Mat & _inMat, Mat& _outMat)
     }
 
     else if (type == 1) {
-// Use openCv mask API.
-double t = getTickCount();
-Mat kernel = (Mat_<char>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-filter2D(_inMat, _outMat, _inMat.depth(), kernel);
-double timeConSume = (getTickCount() - t) / getTickFrequency();
-qDebug() << "System API mask run time is " << timeConSume ;
+        // Use openCv mask API.
+        double t = getTickCount();
+        Mat kernel = (Mat_<char>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+        filter2D(_inMat, _outMat, _inMat.depth(), kernel);
+        double timeConSume = (getTickCount() - t) / getTickFrequency();
+        qDebug() << "System API mask run time is " << timeConSume ;
     }
 }
 
-void testPixel( Mat & _inMat, Mat& _outMat)
+void testPixel(const Mat & _inMat, Mat& _outMat)
 {
 //    int width = _inMat.cols;
 //    int height = _inMat.rows;
@@ -206,9 +122,9 @@ void testChannels(Mat& _inMat)
     //利用vector对象分离
     split(_inMat, channels);
 
-//    BImg = aChannels[0];
-//    GImg = aChannels[1];
-//    RImg = aChannels[2];
+    //    BImg = aChannels[0];
+    //    GImg = aChannels[1];
+    //    RImg = aChannels[2];
 
     BImg = channels[0];
     GImg = channels[1];
@@ -222,11 +138,11 @@ void testChannels(Mat& _inMat)
     Mat GIamgT = Mat::zeros(BImg.size(), BImg.type());
     Mat RIamgT = Mat::zeros(BImg.size(), BImg.type());
 
-//   当每个通道存在时，才会显示为彩色 通道分离后的单通道只显示灰色
-//    std::vector<Mat> aChannelss(3);
-//    aChannelss[0] = BImg;
-//    aChannelss[1] = GIamgT;
-//    aChannelss[2] = RIamgT;
+    //   当每个通道存在时，才会显示为彩色 通道分离后的单通道只显示灰色
+    //    std::vector<Mat> aChannelss(3);
+    //    aChannelss[0] = BImg;
+    //    aChannelss[1] = GIamgT;
+    //    aChannelss[2] = RIamgT;
     //merge功能：将一些数组合并成一个多通道数组
     Mat dst;
     merge(channels, dst);
@@ -324,87 +240,81 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
 
-    FqImageMat imageMat(cv::String("D:/Data/Code_Workspace/imagesTestEx/test2.jpg"), IMREAD_UNCHANGED);
+    FqImageMat imageMat(cv::String("D:/Data/Code_Workspace/imagesTestEx/test.jpg"), IMREAD_UNCHANGED);
 
     if (imageMat.empty()){
         qDebug() << "imageMat is empty!" ;
         return ;
     }
 
+    FqImageMat::setLabelPixmap(ui->label, imageMat, this->width()/2, this->height());
 
-    FqImageMat::show("test1", imageMat.getImage());
-    FqImageMat::show("test2", imageMat.getMat());
-    FqImageMat::show("test3", imageMat);
+    //     //cvtColor
+    //        Mat outStr ;
+    //        cvtColor(imageMat.mat, outStr, CV_RGB2HLS_FULL);
+    //        FqImageMat::setLabelPixmap(ui->label2, outStr, this->width()/2, this->height());
 
-    ui->label->setPixmap(QPixmap::fromImage(imageMat.getImage()));
+    //     //mask  for Mat.ptr()
+    //        Mat maskOutMat = Mat::zeros(imageMat.mat.size(), imageMat.mat.type());
+    //        testMask(0, imageMat.mat, maskOutMat);
+    //        FqImageMat::setLabelPixmap(ui->label2, maskOutMat,  this->width()/2, this->height());
 
-    // cvtColor
-//    Mat outStr ;
-//    cvtColor(str, outStr, CV_RGB2HLS);
+    //     //get Pixel with three or one channels
+    //        Mat pixelMat = Mat::zeros(imageMat.mat.size(), imageMat.mat.type());
 
-//    QImage outStrQImage = cvMatToQImage(outStr);
-//    ui->label2->setPixmap(QPixmap::fromImage(outStrQImage));
+    //        // 注意通道数匹配 否则导致处理完的图像看起来像被压缩过
+    //        Mat pixelMat2 = Mat::zeros(imageMat.mat.size(), CV_8UC1);
+    //        cvtColor(imageMat.mat, pixelMat, CV_RGB2GRAY, 1);
+    //        testPixel(imageMat.mat, pixelMat2);  // three channels
+    //        //testPixel(pixelMat, pixelMat2);  // one channels
+    //        FqImageMat::setLabelPixmap(ui->label2, pixelMat2,  this->width()/2, this->height());
 
-    // imwrite
-//    imwrite("D:/Data/Code_Workspace/imagesTestEx/QtHLS.png", outStr);
+    //     //图像混合  G(x)=(1-a)F(x)+aQ(x)
+    //        FqImageMat str1 = FqImageMat("D:/Data/Code_Workspace/imagesTestEx/CMD.jpg", IMREAD_UNCHANGED);
+    //        FqImageMat str2 = FqImageMat("D:/Data/Code_Workspace/imagesTestEx/git.jpg", IMREAD_UNCHANGED);
+    //        if (str1.empty()){
+    //            qDebug() << "str1 is empty!" ;
+    //            return ;
+    //        }
+    //        if (str2.empty()){
+    //            qDebug() << "str2 is empty!" ;
+    //            return ;
+    //        }
+    //        Mat outStr;
+    //        FqImageMat::setLabelPixmap(ui->label, str1,  this->width()/2, this->height());
+    //        FqImageMat::setLabelPixmap(ui->label2, str2,  this->width()/2, this->height());
+    //        double alpha = 0.5;
+    //        // 要保证需要混合的图像尺寸和类型一致
+    //        if (str1.mat.size() == str2.mat.size() && str1.mat.type() == str2.mat.type()){
+    //            // addWeighted(str1.mat, alpha, str2.mat, 1-alpha, 0.0,outStr);
+    //            //add(str1.mat, str2.mat, outStr);
+    //             multiply(str1.mat, str2.mat, outStr);
+    //            imshow("test", outStr);
+    //        }
 
+    //     //图像对比度和亮度操作  G(x) = aF(x) + b // a影响对比度 b影响亮度
+    //        Mat testMat;
 
-    //setLabelPixmap(ui->label, str, this->width()/2, this->height());
+    //        // 三通道图像操作
+    //        testMat = Mat::zeros(imageMat.mat.size(), imageMat.mat.type());
+    //        Mat threeChannelMat = imageMat.mat;
+    //        testBrightnessContrast(threeChannelMat, testMat);
 
-    // mask  for Mat.ptr()
-//    Mat maskOutMat = Mat::zeros(str.size(), str.type());
-//    testMask(1, str, maskOutMat);
-//    setLabelPixmap(ui->label2, maskOutMat,  this->width()/2, this->height());
+    //        // 单通道图像操作
+    //        // testMat = Mat::zeros(imageMat.mat.size(), CV_8UC1);
+    //        //Mat oneChannelMat;
+    //        //cvtColor(imageMat.mat, oneChannelMat, CV_RGB2GRAY);
+    //        //testBrightnessContrast(oneChannelMat, testMat);
+    //        FqImageMat::setLabelPixmap(ui->label2, testMat,  this->width()/2, this->height());
 
-    // get Pixel with three or one channels
-   //  Mat pixelMat = Mat::zeros(str.size(), str.type());
+    //    //分离颜色通道，多通道融合
+    //    testChannels(imageMat.mat);
 
-    // 注意通道数匹配 否则导致处理完的图像看起来像被压缩过
-//    Mat pixelMat2 = Mat::zeros(str.size(), CV_8UC1);
-//    cvtColor(str, pixelMat, CV_RGB2GRAY, 1);
-//    testPixel(str, pixelMat2);  // three channels
-//    //testPixel(pixelMat, pixelMat2);  // one channels
-//    setLabelPixmap(ui->label2, pixelMat2,  this->width()/2, this->height());
+    //    // openCv绘制图形有文字
+    //    testGraphicsText(imageMat.mat);
 
-    // 图像混合  G(x)=(1-a)F(x)+aQ(x)
-//    Mat str1 = imread("D:/Data/Code_Workspace/imagesTestEx/CMD.jpg", IMREAD_UNCHANGED);
-//    Mat str2 = imread("D:/Data/Code_Workspace/imagesTestEx/git.jpg", IMREAD_UNCHANGED);
-//    if (str1.empty()){
-//        qDebug() << "str1 is empty!" ;
-//        return ;
-//    }
-//    if (str2.empty()){
-//        qDebug() << "str2 is empty!" ;
-//        return ;
-//    }
-//    Mat outStr;
-//    setLabelPixmap(ui->label, str1,  this->width()/2, this->height());
-//    setLabelPixmap(ui->label2, str2,  this->width()/2, this->height());
-//    double alpha = 0.5;
-//    // 要保证需要混合的图像尺寸和类型一致
-//    if (str1.size() == str2.size() && str1.type() == str2.type()){
-//        //addWeighted(str1, alpha, str2, 1-alpha, 0.0,outStr);
-//        // add(str1, str2, outStr);
-//        multiply(str1, str2, outStr);
-//        imshow("test", outStr);
-//    }
-
-    // 图像对比度和亮度操作  G(x) = aF(x) + b // a影响对比度 b影响亮度
-//    Mat testMat;
-//    //testMat = Mat::zeros(str.size(), str.type());
-//    testMat = Mat::zeros(str.size(), CV_8UC1);
-//    cvtColor(str, str, CV_RGB2GRAY);
-//    testBrightnessContrast(str,testMat);
-//    setLabelPixmap(ui->label2, testMat,  this->width()/2, this->height());
-
-    //分离颜色通道，多通道融合
-    //testChannels(str);
-
-    // openCv绘制图形有文字
- //   testGraphicsText(str);
-
-    // openCv  图像模糊
-    //testBlur(str);
+    //    // openCv  图像模糊
+    //    testBlur(imageMat.mat);
 
 }
 
--- openCvTest.pro
+++ openCvTest.pro
@@ -9,15 +9,11 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    BasicComponents/TypesExt.cpp \
-    BasicComponents/Value.cpp \
     FqImageMat.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    BasicComponents/TypesExt.hpp \
-    BasicComponents/Value.hpp \
     FqImageMat.hpp \
     mainwindow.hpp
 
